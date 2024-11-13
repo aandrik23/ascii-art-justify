1bc5191d91607dd10ed90ec62da9be3473cc274d
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-11-14 00:24:07 +0200

    finaljustify

--- main.go
+++ main.go
@@ -4,21 +4,38 @@ import (
 	"fmt"
 	"justify/funcs"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
-
-	"golang.org/x/term"
 )
+
+func getTerminalWidth() (int, error) {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
+	if err != nil {
+		return 0, fmt.Errorf("could not get terminal width")
+	}
+
+	// Output format: "<rows> <columns>"
+	parts := strings.Fields(string(out))
+	if len(parts) < 2 {
+		return 0, fmt.Errorf("unexpected output from stty size")
+	}
+
+	width, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse terminal width")
+	}
+	return width, nil
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("ERROR! Usage: go run . [OPTION] [STRING] [BANNER] \nEX: go run . --align=<TYPE> something standard ")
 		return
 	}
-	// Check if the number of command-line arguments is correct
-	if len(os.Args) > 4 {
-		fmt.Printf("ERROR! Usage: go run . [OPTION] [STRING] [BANNER] \nEX: go run . --align=%v something standard \n", os.Args[2])
-		return
-	}
+
 	var text string
 	var align string
 	styleBanner := "standard"
@@ -41,12 +58,10 @@ func main() {
 	if len(os.Args) == 3 {
 		align = os.Args[1]
 		if strings.HasPrefix(align, "--align=") {
-
 			text = os.Args[2]
 			align = align[8:] // Skips the "--align=" part
 			hasAlign = true
 		} else {
-
 			text = os.Args[1]
 			styleBanner = strings.ToLower(os.Args[2])
 		}
@@ -65,28 +80,24 @@ func main() {
 
 	file, err := os.ReadFile("banners/" + styleBanner + ".txt")
 	if err != nil {
-		fmt.Println(" banner does not exist.")
+		fmt.Println("banner does not exist.")
 		return
 	}
 
-	width, _, err := term.GetSize(int(os.Stdout.Fd()))
+	width, err := getTerminalWidth()
 	if err != nil {
-		fmt.Println("Error getting terminal size:", err)
+		fmt.Println("Error getting terminal width:", err)
 		return
 	}
-
-	// fmt.Println("Max terminal width:", width)
 
 	str := string(file)
 	str = strings.Replace(str, "\r\n", "\n", -1)
 	lines := strings.Split(str, "\n")
 	if !hasAlign {
-
 		funcs.PrintAsciiArt(sepText, lines)
 	} else if align == "justify" {
 		funcs.PrintJustify(sepText, lines, align, width)
 	} else {
 		funcs.PrintAlign(sepText, lines, align, width)
 	}
-
 }
